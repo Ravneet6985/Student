f29b9824c99ee7ca066c1f8b8b2502ed735bd20f
Ravneet <kaur6985@sheridancollege.ca>
2019-05-16 13:39:22 -0400

    initial commit

--- src/design/Design.java
+++ src/design/Design.java
@@ -15,7 +15,7 @@ public class Design {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hrllo");
+        System.out.println("hello");
         // TODO code application logic here
     }
     
